db3d390f0a42bfb0f798ace3ebda954cce26fddf
ronan magalhaes <ronangmsrs@gmail.com>
2023-02-27 11:19:46 -0300

    primeiro commit

---
+++ app/src/main/java/com/example/challenge/bottom_bar/BottomBar.kt
@@ -0,0 +1,13 @@
+package com.example.challenge.bottom_bar
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.challenge.R
+
+class BottomBar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_bar)
+
+    }
+}
